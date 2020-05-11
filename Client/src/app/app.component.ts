b7856cd74f68b19d89ba3657f21751e8f62f1107
Guy Zion <guyzion7@gmail.com>
2020-05-11 16:20:28 +0300

    Add custom markers

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { tileLayer, latLng, circle, polygon, marker } from 'leaflet';
+import { tileLayer, latLng, circle, polygon, marker, icon, layerGroup, Map } from 'leaflet';
+import 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/images/marker-icon.png';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +10,52 @@ import { tileLayer, latLng, circle, polygon, marker } from 'leaflet';
 })
 export class AppComponent {
   title = 'Client';
+
+  overlayGroup = layerGroup();
+
+  allOverlays = [
+    marker([32, 35], {
+      icon: icon({
+         iconSize: [50, 56],
+         iconAnchor: [25, 56],
+         iconUrl: 'assets/leumit-open.png',
+      })
+    }),
+    marker([32, 34.8], {
+      icon: icon({
+         iconSize: [50, 56],
+         iconAnchor: [25, 56],
+         iconUrl: 'assets/klalit-closed.png',
+      })
+    }),
+    marker([31.8, 35], {
+      icon: icon({
+         iconSize: [ 50, 56 ],
+         iconAnchor: [25, 56],
+         iconUrl: 'assets/meuhedet-open.png',
+      })
+    }),
+  ]
+  
   options = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }), 
+      this.overlayGroup,
     ],
     zoom: 10,
     center: latLng(31.9692438, 34.8382341)
   };
+
   layersControl = {
-    baseLayers: {
-      'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-      'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    },
     overlays: {
-      'Big Marker': marker([32, 35]),
-      'Big Circle': circle([ 32, 35 ], { radius: 5000 }),
-      'Big Square': polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
+      'Clinics': this.overlayGroup,
     }
+  }
+
+  onMapReady(map: Map){
+    this.allOverlays.forEach(overlay => {
+      overlay.bindPopup("<b>קופת חולים כללית</b><br>פתוחים");
+      overlay.addTo(this.overlayGroup)
+    });
   }
 }
